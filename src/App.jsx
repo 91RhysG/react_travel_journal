5b91b07c4cd83e88f4fe0a3915bb2b2c7e446137
91RhysG <rhys.gillham.101@outlook.com.au>
2023-01-27 14:50:13 +0800

    Comments added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Nav from "./components/Nav";
 import JournalEntry from "./components/JournalEntry";
 import data from "./data/data.js";
 
-function App() {
+function App()
+{
+  //This go through the set of data and converts it from raw data to the component, '...entry' passes down the data and does not add a new layer
   const entries = data.map((entry, id) => <JournalEntry key={id} {...entry} />);
 
   return (
     <div className="App">
       <Nav />
+      {/* Below is the set of data that is going to be displayed, it iterates and renders it automatically */}
       {entries}
     </div>
   );
